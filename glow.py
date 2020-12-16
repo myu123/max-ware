49deacc52f25f9fb00197082e0d91e1b6db5ea67
myu123 <maxyu.usa.6@gmail.com>
2020-12-16 16:32:44 -0500

    Glow ESP

---
+++ glow.py
@@ -0,0 +1,39 @@
+import pymem
+import pymem.process
+from cs_offsets import *
+
+def glow():
+    print("MaxWare has been launched!")
+    pm = pymem.Pymem("csgo.exe")
+    client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll
+
+    while 1:
+        glow_manager = pm.read_int(client + dwGlowObjectManager)
+
+        # Entities 1-32 are reserved for players.
+        for i in range(1, 32):  
+            entity = pm.read_int(client + dwEntityList + i * 0x10)
+
+            if entity:
+                entity_team_id = pm.read_int(entity + m_iTeamNum)
+                entity_glow = pm.read_int(entity + m_iGlowIndex)
+
+                # Terrorist
+                if entity_team_id == 2:  
+                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x4, float(1))   # R 
+                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x8, float(0))   # G
+                    pm.write_float(glow_manager + entity_glow * 0x38 + 0xC, float(0))   # B
+                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x10, float(1))  # Alpha
+                    pm.write_int(glow_manager + entity_glow * 0x38 + 0x24, 1)           # Enable glow
+
+                # Counter-terrorist
+                elif entity_team_id == 3:  
+                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x4, float(0))   # R
+                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x8, float(0))   # G
+                    pm.write_float(glow_manager + entity_glow * 0x38 + 0xC, float(1))   # B
+                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x10, float(1))  # Alpha
+                    pm.write_int(glow_manager + entity_glow * 0x38 + 0x24, 1)           # Enable glow
+
+
+if __name__ == '__main__':
+    glow()
