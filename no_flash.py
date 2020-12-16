5dfb23ddd0ed8cd841d15e275648992d181464e6
myu123 <71144948+myu123@users.noreply.github.com>
2020-12-16 15:25:10 -0500

    Add files via upload

---
+++ no_flash.py
@@ -0,0 +1,19 @@
+import pymem
+import pymem.process
+from cs_offsets import *
+
+def no_flash():
+    print("MaxWare has launched!")
+    pm = pymem.Pymem("csgo.exe")
+    client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll
+
+    while True:
+        player = pm.read_int(client + dwLocalPlayer)
+        if player:
+            flash_value = player + m_flFlashMaxAlpha
+            if flash_value:
+                pm.write_float(flash_value, float(0))
+
+
+if __name__ == '__main__':
+    no_flash()
